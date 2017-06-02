47ef196239f9faf8028f25b5925055d38930808e
andrewthebeast <andrewr.luna@yahoo.com>
2017-06-01 23:39:30 -0500

    Update Dataplot.py

--- DataVisual/Dataplot.py
+++ DataVisual/Dataplot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 #I made my own data set it is small but it works
+#The data comes from this website https://smartasset.com/retirement/the-average-salary-by-age
 age = [16,20,25, 35, 45, 55, 65]
 income = [420, 528, 758, 950, 962, 954, 888]
 plt.title('Average income per age')
@@ -8,5 +9,3 @@ plt.ylabel('Income in Dollars')
 plt.plot(age, income, color = 'red')
 plt.axes([16, 65, 400, 1000])
 plt.show()
-
-
